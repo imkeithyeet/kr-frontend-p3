f8fee8ae5a3bcaa4bcdda9fd79bd3e84ca0e04ce
Ryan See <ryanjosephsee@gmail.com>
2023-01-05 18:57:23 -0800

    no

---
+++ src/components/pages/EditReview.js
@@ -0,0 +1,37 @@
+
+import React, { useState } from "react";
+
+function EditReview({ id, body, onUpdateReview }) {
+  const [reviewBody, setReviewBody] = useState(body);
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+
+    fetch(`http://localhost:9292/reviews/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        body: reviewBody,
+      }),
+    })
+      .then((r) => r.json())
+      .then((updatedReview) => onUpdateReview(updatedReview));
+  }
+
+  return (
+    <form className="edit-review" onSubmit={handleFormSubmit}>
+      <input
+        type="text"
+        name="body"
+        autoComplete="off"
+        value={reviewBody}
+        onChange={(e) => setReviewBody(e.target.value)}
+      />
+      <input type="submit" value="Save" />
+    </form>
+  );
+}
+
+export default EditReview;
--- src/components/pages/Review.js
+++ src/components/pages/Review.js
@@ -1,3 +1,138 @@
+// import React,{useState, useEffect} from "react";
+// import ReviewList from "./ReviewList"
+// // import './Review.css'
+
+// const API="http://localhost:9393/reviews"
+
+// function Review() {
+//   const [items, setItems] = useState([]);
+//   const [editing, setEditing] = useState(false);
+//   const [currentReview, setCurrentReview] = useState({ id: null, name: '', description: '', rating: "" });
+
+//   const addReview = (review) => {
+//     fetch('http://localhost:9393/reviews', {
+//     method: 'POST',
+//     body: JSON.stringify(review),
+//     headers: {
+//     'Content-Type': 'application/json'
+//     }
+//     })
+//     .then((response) => response.json())
+//     .then((newReview) => {
+//     setItems([...items, newReview]);
+//     });
+//     };
+    
+//     const deleteReview = (id) => {
+//       fetch(`http://localhost:9393/reviews/${id}`, {
+//       method: 'DELETE',
+//       })
+//       .then(() => {
+//       const remainingReviews = items.filter((r) => r.id !== id);
+//       setItems(remainingReviews);
+//       });
+//       };
+      
+// const editReview = (review) => {
+//   fetch(`http://localhost:9393/reviews/${review.id}`, {
+//   method: 'PATCH',
+//   body: JSON.stringify(review),
+//   headers: {
+//   'Content-Type': 'application/json'
+//   }
+//   })
+//   .then((response) => response.json())
+//   .then((updatedReview) => {
+//   const updatedReviews = items.map((r) =>
+//   r.id === updatedReview.id ? updatedReview : r
+//   );
+//   setItems(updatedReviews);
+//   });
+//   };
+
+// const updateReview = (id, updatedReview) => {
+//   setEditing(false);
+//   setItems(items.map(review => (review.id === id ? updatedReview : review)));
+// }
+
+//   useEffect(() =>{
+//     fetch(API)
+//     .then(res=>res.json())
+//     .then (data=>setItems(data))
+//   },[])
+// console.log(items)
+
+
+//     return (
+
+//       <div>
+//         <div className="review-container">
+//         <h1 className="r" style={{color: "blue"}}>Reviews</h1>
+//         {editing ? (
+//         <div>
+//           <form  onSubmit={e => {
+//           e.preventDefault();
+//           updateReview(currentReview.id, currentReview);
+//           }}>
+//               <label>Name:</label>
+//               <input  type="text" name="name" value={currentReview.name} onChange={e => setCurrentReview({ ...currentReview, name: e.target.value })} />
+//               <label className="P">Description:</label>
+//               <input type="text" name="description" value={currentReview.description} onChange={e => setCurrentReview({ ...currentReview, description: e.target.value })} />
+//               <label>Rating:</label>
+//               <input type="text" name="rating"  value={currentReview.rating} onChange={e => setCurrentReview({ ...currentReview, rating: e.target.value })} />
+//               </form>
+//               </div>
+//               ) : (
+//               <div className="stuff">
+//               <form onSubmit={e => {
+//               e.preventDefault();
+//               addReview({ name: e.target.name.value, description: e.target.description.value, rating: e.target.rating.value});
+//               e.target.name.value = '';
+//               e.target.description.value = '';
+//               e.target.rating.value = '';
+//               }}>
+//               <label>Name:</label>
+//               <input placeholder="name..." type="text" name="name" />
+//               <label>Description:</label>
+//               <input placeholder="description..." type="text" name="description" />
+//               <label>Rating:</label>
+//               <input placeholder="rating..." type="text" name="rating" />
+//               <div className="Submit">
+//               <button  type="submit">Add Review</button>
+//               </div>
+//           </form>
+//           </div>
+//           )}
+//           <div style={{
+//             borderWidth: '3px',
+//             borderStyle: 'solid',
+//             borderColor: 'black',
+//             padding: '50px'
+//           }}>
+//           {items.map(review => (
+//           <div  style={{
+//             borderWidth: '2px',
+//             borderStyle: 'solid',
+//             borderColor: 'black',
+//             padding: '50px'
+//           }} key={review.id}>
+//           <h3>{review.name}</h3>
+//           <p>{review.description}</p>
+//           <p>{review.rating}</p>
+//           <button onClick={() => deleteReview(review.id)}>Delete</button>
+//           <button onClick={() => editReview(review)}>Edit</button>
+//           </div>
+//           ))}
+//           </div>
+//     </div>
+//         <ReviewList className="reviewList" items={items} />
+//   </div>
+//   );
+// }
+  
+//   export default Review;
+
+
 import React,{useState, useEffect} from "react";
 import ReviewList from "./ReviewList"
 // import './Review.css'
@@ -22,6 +157,7 @@ function Review() {
     setItems([...items, newReview]);
     });
     };
+    
     const deleteReview = (id) => {
       fetch(`http://localhost:9393/reviews/${id}`, {
       method: 'DELETE',
@@ -31,6 +167,7 @@ function Review() {
       setItems(remainingReviews);
       });
       };
+      
 const editReview = (review) => {
   fetch(`http://localhost:9393/reviews/${review.id}`, {
   method: 'PATCH',
@@ -78,8 +215,6 @@ console.log(items)
               <input type="text" name="description" value={currentReview.description} onChange={e => setCurrentReview({ ...currentReview, description: e.target.value })} />
               <label>Rating:</label>
               <input type="text" name="rating"  value={currentReview.rating} onChange={e => setCurrentReview({ ...currentReview, rating: e.target.value })} />
-
-              <button type="submit">Update Review</button>
               </form>
               </div>
               ) : (
